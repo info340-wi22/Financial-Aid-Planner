dba0aedbc9d209fd280d01bf6d3d128dc644c7b7
Brian Ho <brianho34@yahoo.com>
2022-02-16 14:48:42 -0800

    react for tasks under navbar

--- src/App.js
+++ src/App.js
@@ -7,3 +7,68 @@ export default function App() {
 	</div>
 	)
 }
+
+
+export function cost(props) {
+    const cost= props.cost;
+    return (
+        <div>
+            <input type="text" cost={props.cost} />
+        </div>
+    )
+}
+export function amountCover(props) {
+    const amountCover= props.amountCover;
+
+    return (
+        <div>
+            <input type="text" amountCover={props.amountCover} />
+        </div>
+    )
+}
+
+
+export function currentAid(props) {
+    return (
+        <ReactComponent>
+            <form>
+                <input
+                    type="text"
+                    name="value"
+                    value={props.state.value}
+                    currentAid={props.currentAid}
+                />
+            </form>
+            </ReactComponent>
+    )
+}
+export function potentialAid(props) {
+    return (
+        <ReactComponent>
+            <form>
+                <input
+                    type="text"
+                    name="value"
+                    value={props.state.value}
+                    potentialAid={props.potentialAid}
+                />
+            </form>
+            </ReactComponent>
+    )
+}
+
+export function amountLeft(props) {
+    return (
+        <ReactComponent>
+            <form>
+                <input
+                    type="text"
+                    name="value"
+                    value={props.state.username}
+                    potentialAid={props.amountLeft}
+                />
+            </form>
+        </ReactComponent>
+    )
+}
+
