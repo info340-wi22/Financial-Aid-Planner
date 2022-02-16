1bd1b6cb0ba1bf03fd6de9ff1ddee7a427ef40e9
William Siauw <36526721+Volcore001@users.noreply.github.com>
2022-02-16 13:47:03 -0800

    Added Navbar (#22)

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+export default function NavBar() {
+	return (<div className="nav">
+			<header>
+				<nav class="menu">
+					<a href="index.html">Current Plan</a>
+					<a href="">Plans</a>
+					<a href="explore.html" target="_blank">Explore</a>
+				</nav>
+
+				<p>Learn, Not Spend</p>
+			</header>
+			</div>)
+}
