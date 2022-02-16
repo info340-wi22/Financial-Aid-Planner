ecdf5ab7c8f32542d3ac3b46b96798ca9876da19
William Siauw <billsiauw@gmail.com>
2022-02-16 13:58:03 -0800

    Styling fix

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,14 @@
 export default function NavBar() {
-	return (<div className="nav">
-			<header>
-				<nav class="menu">
-					<a href="index.html">Current Plan</a>
-					<a href="">Plans</a>
-					<a href="explore.html" target="_blank">Explore</a>
-				</nav>
+	return (
+	<div className="nav">
+		<header>
+			<nav class="menu">
+				<a href="index.html">Current Plan</a>
+				<a href="">Plans</a>
+				<a href="explore.html" target="_blank">Explore</a>
+			</nav>
 
-				<p>Learn, Not Spend</p>
-			</header>
-			</div>)
+			<p>Learn, Not Spend</p>
+		</header>
+	</div>)
 }
