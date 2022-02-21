a3623cafa1d1a36e59b8d6f5e6e145b8d35854f4
btho1 <btho1@uw.edu>
2022-02-20 16:14:12 -0800

    progress for userinput

---
+++ src/UserInput.js
@@ -0,0 +1,31 @@
+import NavBar from './NavBar';
+
+export default function App() {
+	return (
+	<div>
+		<NavBar />
+	</div>
+	)
+}
+
+function Cost() {
+	const userData = ['userCost', 'schloarshipCost'];
+
+	click = () => {
+		console.log("The cost of your tuiton per year is" + Cost)
+	}
+return (
+	DataTransfer={data} 
+)
+}
+	
+
+
+function amount(props) {
+	let amountNeeded = props.DataTransfer; //data from schools and schlorship information per school
+}
+
+	return 
+	<div>
+		<button click={findAmountNeeded}></button>
+	</div>
