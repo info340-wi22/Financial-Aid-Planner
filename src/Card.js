c7128843c0fceb2f6ec987c900ee583a2f553738
ZacZhang <zachaz@uw.edu>
2022-02-20 16:55:42 -0800

    Initial Cards code

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,58 @@
 import React from 'react';
+
+export default function Card() {
+  return (
+    <div className='card'>
+      <Title/>
+      <Status/>
+      <ToDo/>
+      <Amount/>
+    </div>
+  );
+}
+
+function Title() {
+  return (
+    <div className='title'>
+      <p>Scholarship</p>
+      <a href=''>Link to Scholarship</a>
+    </div>
+  );
+}
+
+function Status() {
+  return (
+    <div className='status accepted'>
+      <p>Current Status: Acctped</p>
+    </div>
+  );
+}
+
+function ToDo() {
+  return (
+    <div className='to-do'>
+      <CheckBox/>
+      <CheckBox/>
+      <CheckBox/>
+    </div>
+  );
+}
+
+function CheckBox() {
+  return (
+    <div className='checkbox-wrapper'>
+      <input type="checkbox" name='cardbox'></input>
+      <label htmlFor='cardboox'>Get A letter of rec</label>
+    </div>
+  );
+}
+
+function Amount() {
+  return (
+    <div className='amount'>
+      <p>Frequency Per Year: 3</p>
+      <p>Amount Paid Per Frequency: 3000</p>
+      <p>Total Amount Per Year</p>
+    </div>
+  );
+}
