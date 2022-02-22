08ab739955fd926f3805b15e4156fe414270c0bd
Zac <zachaz@uw.edu>
2022-02-21 16:03:31 -0800

    Add Cards (#34)
    * add lintr
    
    * remove firebase analytics
    
    * update lint rules
    
    * update lint
    
    * add .env file with DISABLE eslint flag to
    prevent eslint from stopping react compile
    
    * format index.html
    
    * tweaked html head
    
    * removed css link for our stylesheet because we use
    to import our css
    
    * re-add footer and remove nonsemantic small tag
    
    * modify eslint rules
    
    * fix footer placement
    
    * re-add footer and remove nonsemantic small tag
    
    * beginning work on cards
    
    * remove dangling merge error
    
    * delete card
    
    * Initial Cards code
    
    * refromat file
    
    * clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import NavBar from './NavBar';
 import React from 'react';
+import {CardList} from './CardList';
 
 export default function App() {
   return (
     <div>
-      <NavBar />
+      <div>
+        <NavBar/>
+      </div>
+      <div className='plan'>
+        <CardList/>
+      </div>
     </div>
   );
 }
