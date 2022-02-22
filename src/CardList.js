b5707e0a6e65215ca44f8d7241c9d808bc9400a1
ZacZhang <zachaz@uw.edu>
2022-02-21 19:01:48 -0800

    clean up code

--- src/CardList.js
+++ src/CardList.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Card from './Card';
 
 export function CardList(props) {
-  const cards = props.cardList.map((card) => {
+  const {cardList} = props;
+  const cards = cardList.map((card) => {
     const key = 'card' + card.key;
     return <div key={key}><Card name={card.name} status={card.cardStatus} toDo={card.toDo} amount={card.amount}/></div>;
   });
