e0531d1935eeee82621c500c7c4f027f508fbe98
btho1 <72411050+btho1@users.noreply.github.com>
2022-02-21 16:33:19 -0800

    Brian branch (#37)
    * react for tasks under navbar
    
    * progress for userinput
    
    * userinput section
    
    Co-authored-by: Brian Ho <brianho34@yahoo.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import NavBar from './NavBar';
 import React from 'react';
-import {CardList} from './CardList';
+import { CardList } from './CardList';
+import UserInput from './UserInput'
 
-export default function App() {
-  return (
-    <div>
-      <div>
-        <NavBar/>
-      </div>
-      <div className='plan'>
-        <CardList/>
-      </div>
-    </div>
-  );
+function App() {
+    const addPost = (costPerYear, amountCover, currentAid, potentialAid, amountLeft) => {
+        const newPost = {
+            yearlyCost: costPerYear,
+            userCover: amountCover,
+            userAid: currentAid,
+            userPotential: potentialAid,
+            userAmountNeed: amountLeft
+        }
+        console.log(newPost);
+    }
+    return (
+        <div className='container'>
+            <NavBar />
+            <main>
+                <UserInput onSumbit={addPost} />
+                <div className='plan'>
+                    <CardList />
+                </div>
+            </main>
+        </div>
+    );
 }
+export default App;
