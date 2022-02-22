02bedda4e8ccd02ace5409b853b747d6f4688fd7
btho1 <btho1@uw.edu>
2022-02-21 16:09:41 -0800

    userinput section

--- src/App.js
+++ src/App.js
@@ -1,74 +1,26 @@
 import NavBar from './NavBar';
+import UserInput from './UserInput'
 
-export default function App() {
+function App() {
+    const addPost = (costPerYear, amountCover, currentAid, potentialAid, amountLeft) => {
+        const newPost = {
+            yearlyCost: costPerYear, 
+            userCover: amountCover,
+            userAid: currentAid,
+            userPotential: potentialAid,
+            userAmountNeed: amountLeft
+        }
+        console.log(newPost);
+    }
 	return (
-	<div>
-		<NavBar />
-	</div>
-	)
-}
-
-
-export function cost(props) {
-    const cost= props.cost;
-    return (
-        <div>
-            <input type="text" cost={props.cost} />
+        <div className='container'>
+            <NavBar />
+            <main>
+        <UserInput onSumbit={addPost} />
+	</main>
         </div>
-    )
-}
-export function amountCover(props) {
-    const amountCover= props.amountCover;
-
-    return (
-        <div>
-            <input type="text" amountCover={props.amountCover} />
-        </div>
-    )
+	);
 }
 
-
-export function currentAid(props) {
-    return (
-        <ReactComponent>
-            <form>
-                <input
-                    type="text"
-                    name="value"
-                    value={props.state.value}
-                    currentAid={props.currentAid}
-                />
-            </form>
-            </ReactComponent>
-    )
-}
-export function potentialAid(props) {
-    return (
-        <ReactComponent>
-            <form>
-                <input
-                    type="text"
-                    name="value"
-                    value={props.state.value}
-                    potentialAid={props.potentialAid}
-                />
-            </form>
-            </ReactComponent>
-    )
-}
-
-export function amountLeft(props) {
-    return (
-        <ReactComponent>
-            <form>
-                <input
-                    type="text"
-                    name="value"
-                    value={props.state.username}
-                    potentialAid={props.amountLeft}
-                />
-            </form>
-        </ReactComponent>
-    )
-}
+export default App;
 
--- src/UserInput.js
+++ src/UserInput.js
@@ -1,31 +1,41 @@
-import NavBar from './NavBar';
+import React, {useState} from "react";
 
-export default function App() {
-	return (
-	<div>
-		<NavBar />
-	</div>
-	)
-}
-
-function Cost() {
-	const userData = ['userCost', 'schloarshipCost'];
-
-	click = () => {
-		console.log("The cost of your tuiton per year is" + Cost)
+ function UserInput(props) {
+	const [amount, setAmount] = useState("");
+	const handleInputAmount = (event) => {
+		setAmount(event.target.value);
 	}
-return (
-	DataTransfer={data} 
-)
+	const [cover, setCover] = useState("");
+	const handleInputCover = (event) => {
+		setCover(event.target.value);
+	}
+	 const handleSubmit = (event) => {
+		 props.onSubmit(amount, cover, "currentAid", "potentialAid", "amountLeft");
+
+	 }
+	 return(
+			<div className="info">
+            <button type="button" id="arrow-button-l" className="arrow-button"><span className="arrow left"></span>Page 1</button>
+
+            <div className="text" role="status">
+                <form>
+                    <label for="cost">Cost of College Per Year:</label>
+                    <input type="text" id="cost" name="cost" value={amount} onChange={handleInputAmount}> </input>
+                </form>
+                <form>
+                    <label for="cover">Amount You Can Cover:</label>
+                    <input type="text" id="cover" name="cover"  value={cover} onChange={handleInputCover}> </input>
+                </form>
+                <p>Current Aid: 9000</p>
+                <p>Potential Aid: 23000</p>
+                <p>Amount Left: XXXXX</p>
+            </div>
+            <div className="buttons">
+                <button type="submit" id="card-button" onClick={handleSubmit}><span>Add Card</span></button>
+                <button type="submit" id="template-button" onClick={handleSubmit}>Upload As Template</button>
+            </div>
+            <button type="button" id="arrow-button-r" className="arrow-button">Page 3<span className="arrow right"></span></button>
+        </div>
+	 );
 }
-	
-
-
-function amount(props) {
-	let amountNeeded = props.DataTransfer; //data from schools and schlorship information per school
-}
-
-	return 
-	<div>
-		<button click={findAmountNeeded}></button>
-	</div>
+export default UserInput;
