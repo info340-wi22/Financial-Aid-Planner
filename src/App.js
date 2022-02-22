5d5e1ac52ebbfc798c906ffab672f9fd86e02583
Ian Lu <47957926+SamLovesCoding2@users.noreply.github.com>
2022-02-21 17:22:46 -0800

    Ian branch (#38)
    * UserInput
    
    * NavBar
    
    * App.js lint
    
    Co-authored-by: Ian Lu <ianlu@Ians-MacBook-Air.local>

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import NavBar from './NavBar';
 import React from 'react';
-import { CardList } from './CardList';
-import UserInput from './UserInput'
+import {CardList} from './CardList';
+import UserInput from './UserInput';
 
 function App() {
-    const addPost = (costPerYear, amountCover, currentAid, potentialAid, amountLeft) => {
-        const newPost = {
-            yearlyCost: costPerYear,
-            userCover: amountCover,
-            userAid: currentAid,
-            userPotential: potentialAid,
-            userAmountNeed: amountLeft
-        }
-        console.log(newPost);
-    }
-    return (
-        <div className='container'>
-            <NavBar />
-            <main>
-                <UserInput onSumbit={addPost} />
-                <div className='plan'>
-                    <CardList />
-                </div>
-            </main>
+  const addPost = (costPerYear, amountCover, currentAid,
+      potentialAid, amountLeft) => {
+    const newPost = {
+      yearlyCost: costPerYear,
+      userCover: amountCover,
+      userAid: currentAid,
+      userPotential: potentialAid,
+      userAmountNeed: amountLeft,
+    };
+    console.log(newPost);
+  };
+  return (
+    <div className='container'>
+      <NavBar />
+      <main>
+        <UserInput onSumbit={addPost} />
+        <div className='plan'>
+          <CardList />
         </div>
-    );
+      </main>
+    </div>
+  );
 }
+
 export default App;
