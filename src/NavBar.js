0d3b428c7ba470bff421538b957d939b261bfcac
ZacZhang <zachaz@uw.edu>
2022-03-02 16:21:03 -0800

    move header tag into App.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,18 +3,17 @@ import {NavLink} from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <div className="nav">
-      <header>
-        <nav className="menu">
-          <NavLink to="/main">Current Plan</NavLink>
-          <NavLink to="/plan">Plans</NavLink>
-          <NavLink to="/explore">Explore</NavLink>
+    <>
+      <nav className="menu">
+        <NavLink to="/main">Current Plan</NavLink>
+        <NavLink to="/plan">Plans</NavLink>
+        <NavLink to="/explore">Explore</NavLink>
 
-        </nav>
-        <img className="navimage" src="\imgs\learn.png"
-          alt="LearnNotSpend logo" />
-        <h1>Learn, Not Spend</h1>
-        <img className="profile" src=".\imgs\profile-icon.webp" alt="Profile" />
-      </header>
-    </div>);
+      </nav>
+      <img className="navimage" src="\imgs\learn.png"
+        alt="LearnNotSpend logo" />
+      <h1>Learn, Not Spend</h1>
+      <img className="profile" src=".\imgs\profile-icon.webp" alt="Profile" />
+    </>
+  );
 }
