d5812ea94966238f26034ece9df0b4934f21bde1
ZacZhang <zachaz@uw.edu>
2022-03-02 21:59:12 -0800

    Reorganize react components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Route, Routes, Navigate} from 'react-router-dom';
 
-import NavBar from './NavBar';
-import {Main} from './Main';
-import Explore from './Explore';
+import NavBar from './components/nav/NavBar';
+import {Main} from './components/content/plan/Main';
+import Explore from './components/content/explore/Explore';
 
 function App() {
   return (
