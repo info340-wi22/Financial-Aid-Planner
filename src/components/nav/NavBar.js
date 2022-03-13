25771eec1ea72a122bfb8c7e084c87636a6a8427
Zac <zachaz@uw.edu>
2022-03-13 15:41:57 -0700

    Login (#69)
    * add in databaseURL to maybe support realtime database?
    
    * add in react firebase ui dependency for user login
    
    * fix 404 error on refresh of page
    
    * terrible implementation of profile
    
    * Can toggle sign in screen and close it
    
    * update naming
    
    * style close button
    
    * basic responsiveness
    
    * fix react error was using wrong tag for linking
    
    * reformating
    
    * WIP this works locally in react emulator but not in firebase emulator
    
    * terrible implementation of profile
    
    * Can toggle sign in screen and close it
    
    * update naming
    
    * style close button
    
    * basic responsiveness
    
    * fix react error was using wrong tag for linking
    
    * reformating
    
    * WIP this works locally in react emulator but not in firebase emulator
    
    * Firebase sucks
    
    * add login
    
    * basic css styling for logout button

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {Profile} from './Profile';
 
 export default function NavBar() {
   return (
     <>
       <nav className="menu">
-        <NavLink to="/main">Current Plan</NavLink>
-        <NavLink to="/plan">Plans</NavLink>
-        <NavLink to="/explore">Explore</NavLink>
-
+        <Link to="/main">Current Plan</Link>
+        <Link to="/plan">Plans</Link>
+        <Link to="/explore">Explore</Link>
       </nav>
       <img className="navimage" src="\imgs\learn.png"
         alt="LearnNotSpend logo" />
