1d1fc52c89160cc6c9f6fe0e888aedc1281427c9
William Siauw <36526721+Volcore001@users.noreply.github.com>
2022-03-14 12:51:37 -0700

    Will branch (#72)
    * Potential Aid shows proper value
    
    * AMount left now accounts for potential aid, defaults to 0 if negative
    
    * Fixed draft issues
    
    * Explore page pulls plans from Firebase DB
    
    * Explore Plans can be added to a user's plans

--- src/components/content/explore/ExploreCard.js
+++ src/components/content/explore/ExploreCard.js
@@ -1,14 +1,24 @@
 import React from 'react';
-
+import { getDatabase, ref, set as firebaseSet } from 'firebase/database'
 export default function Card(props) {
+	const db = getDatabase();
+	const copyCard = () => {
+		const userRef = ref(db, props.user +"/Plans/"+props.cardInfo.firebaseKey);
+		console.log(props.id);
+		console.log(props.cardInfo);
+		console.log(props.user);
+		firebaseSet(userRef, props.cardInfo)
+		.then(() => console.log("data saved successfully!"))
+		.catch(err => console.log(err)); //log any errors for debugging
+	}
   return (
     <div className="explore-plan">
       <p>TOTAL COST OF COLLEGE PER YEAR: {props.cardInfo.TotalCostCollege}</p>
       <p>AMOUNT WANT TO COVER: {props.cardInfo.AmountCover}</p>
       <p>POTENTIAL AID: {props.cardInfo.PotentialAid}</p>
       <p>College: {props.cardInfo.College}</p>
-      <button type="button" id={'view'+props.cardInfo.key} className="card-button">View</button>
-      <button type="button" id={'copy'+props.cardInfo.key} className="card-button">Copy</button>
+      <button type="button" id={"view"+props.pos} className="card-button" >View</button>
+      <button type="button" id={"copy"+props.pos} className="card-button" onClick={copyCard}>Copy</button>
     </div>
   );
 }
