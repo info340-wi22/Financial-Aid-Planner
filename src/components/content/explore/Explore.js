ee95db87e30fe1bc5e120f4ad9dbee7925731254
ZacZhang <zachaz@uw.edu>
2022-03-13 22:24:27 -0700

    formatting

--- src/components/content/explore/Explore.js
+++ src/components/content/explore/Explore.js
@@ -1,51 +1,49 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 import CardList from './ExploreCardList';
-import { getDatabase, ref, onValue} from 'firebase/database';
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import {getDatabase, ref, onValue} from 'firebase/database';
+import {getAuth, onAuthStateChanged} from 'firebase/auth';
 
 export default function Explore() {
- 
   const auth = getAuth();
   const [user, setUser] = useState(null);
   const [cardList, setCardList] = useState([]);
-onAuthStateChanged(auth, (firebaseUser) => {
-    if(firebaseUser){ //firebaseUser defined: is logged in
-        console.log('logged in', firebaseUser.uid);
-		setUser(firebaseUser.uid);
-        //do something with firebaseUser (e.g. assign to a state variable)
-    }
-    else { //firebaseUser is undefined: is not logged in
-        console.log('logged out');
+  onAuthStateChanged(auth, (firebaseUser) => {
+    if (firebaseUser) { // firebaseUser defined: is logged in
+      console.log('logged in', firebaseUser.uid);
+      setUser(firebaseUser);
+      // do something with firebaseUser (e.g. assign to a state variable)
+    } else { // firebaseUser is undefined: is not logged in
+      console.log('logged out');
     }
   });
   const db = getDatabase();
   useEffect(() => {
-  const userRef = ref(db, "Plans/");
-  const off = onValue(userRef, (snapshot) => {
-	const allPlansObject = snapshot.val(); //get the JSON from the reference
-	const planKeyArray = Object.keys(allPlansObject) 
-	const allPlansArray = planKeyArray.map((keyString) => {
-	const whichObject = {...allPlansObject[keyString], firebaseKey: keyString};
-	  return whichObject;
-	})
+    const userRef = ref(db, 'Plans/');
+    const off = onValue(userRef, (snapshot) => {
+      const allPlansObject = snapshot.val(); // get the JSON from the reference
+      const planKeyArray = Object.keys(allPlansObject);
+      const allPlansArray = planKeyArray.map((keyString) => {
+        const whichObject = {...allPlansObject[keyString], firebaseKey: keyString};
+        return whichObject;
+      });
 
-	//usually save to state
-	setCardList(allPlansArray)
-  })
+      // usually save to state
+      setCardList(allPlansArray);
+    });
 
-  //cleanup function for when component is removed
-  function cleanup() {
-	off(); //turn off all listeners
-  }
-  return cleanup; //effect hook callback returns the cleanup function
-  }, [db])
+    // cleanup function for when component is removed
+    function cleanup() {
+      off(); // turn off all listeners
+    }
+    return cleanup; // effect hook callback returns the cleanup function
+  }, [db]);
   console.log(cardList);
   return (
-	<div className='explore-page'>
-	  <FilterBy/>
-	  <CardList cardList={cardList} user={user}/>
-	</div>
+    <div className='explore-page'>
+      <FilterBy/>
+      <CardList cardList={cardList}/>
+    </div>
   );
 }
 function FilterBy() {
