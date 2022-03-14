fffe8a5b07267c9f2b898a2df0468a8dd42ccff4
Zac <zachaz@uw.edu>
2022-03-13 22:25:14 -0700

    formatting (#71)

--- src/components/content/explore/ExploreCardList.js
+++ src/components/content/explore/ExploreCardList.js
@@ -3,10 +3,10 @@ import React from 'react';
 import Card from './ExploreCard';
 
 export default function CardList(props) {
-  const cards = props.cardList.map((card, index) => <Card cardInfo={card} key={index}/>)
+  const cards = props.cardList.map((card, index) => <Card cardInfo={card} key={index}/>);
   return (
     <div className='plan-card'>
-	{cards}
+      {cards}
     </div>
   );
 }
