8519b9ee827d10f3f37cdfe840483430773ea362
Ian Lu <47957926+SamLovesCoding2@users.noreply.github.com>
2022-03-14 21:34:26 -0700

    Ian plan (#86)
    * fixed lint
    
    * added the plan page
    
    * fixed lint error and implemented set Current button
    
    * fixed exception in plans page if no plan is selected
    
    * fixed merge conflicts
    
    * implemented create a plan
    
    * fixed error
    
    * fixed error
    
    Co-authored-by: Ian Lu <ianlu@Ians-MacBook-Air.local>
    Co-authored-by: William Siauw <36526721+Volcore001@users.noreply.github.com>
    Co-authored-by: Zac <zachaz@uw.edu>

--- src/components/content/explore/ExploreCard.js
+++ src/components/content/explore/ExploreCard.js
@@ -1,22 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import {getDatabase, ref, set as firebaseSet, onValue} from 'firebase/database';
-import {Link, Route, Routes, Navigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 export default function Card(props) {
   const [currentPlan, setCurrentPlan] = useState(true);
 
   const db = getDatabase();
   useEffect(() => {
-    const userRef = ref(db, props.user+'/Current Plan');
+    const userRef = ref(db, props.user + '/Current Plan');
     const off = onValue(userRef, (snapshot) => {
       const allPlansObject = snapshot.val(); // get the JSON from the reference
       if (allPlansObject === null) {
         setCurrentPlan(false);
       } else {
         setCurrentPlan(true);
-      }
+      };
     });
-  });
+    function cleanup() {
+      off(); // turn off all listeners
+    }
+    return cleanup; // effect hook callback returns the cleanup function
+  }, [db, props.loc]);
   const copyCard = () => {
     const userRef = ref(db, props.user + '/Plans/' + props.cardInfo.firebaseKey);
     firebaseSet(userRef, props.cardInfo)
@@ -35,9 +39,8 @@ export default function Card(props) {
       <p>AMOUNT WANT TO COVER: {props.cardInfo.AmountCover}</p>
       <p>POTENTIAL AID: {props.cardInfo.PotentialAid}</p>
       <p>College: {props.cardInfo.College}</p>
-      <Link className="btn btn-primary" to={'/main/'+props.cardInfo.firebaseKey}>Visit</Link>
+      <Link className="btn btn-primary" to={'/main/' + props.cardInfo.firebaseKey}>Visit</Link>
       <button type="button" id={'copy' + props.pos} className="card-button" onClick={copyCard}>Copy</button>
     </div>
   );
 }
-
